af836a1f5099b53801a2134e7b97a39681e9ee23
fantasyemperor <49614039+fantasyemperor@users.noreply.github.com>
2025-10-26 02:28:31 +0800

    tutu
    test

---
+++ src/main/java/cloud/codechun/tutu/TutuApplication.java
@@ -0,0 +1,13 @@
+package cloud.codechun.tutu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/codechun/tutu/TutuApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.codechun.tutu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
